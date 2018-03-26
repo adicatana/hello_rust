4c01bb404ff5afa77ea6bfcef6e9b58529b9a979
Adi Catana <adrian.catana15@imperial.ac.uk>
2018-03-26 19:46:10 +0300

    Basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number.");
+
+    let random_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+
+	    println!("Please enter your guess:");
+
+	    let mut guess = String::new();
+
+	    std::io::stdin().read_line(&mut guess).
+	    	expect("Failed to read line.");
+
+	    println!("You guessed {}", guess);
+
+	    let guess : u32 = guess.trim().parse().
+	    	expect("Faled to convert to number");
+
+	    match guess.cmp(&random_number) {
+	    	std::cmp::Ordering::Less => println!("Too small!"),
+	    	std::cmp::Ordering::Equal => {
+	    		println!("You won!");
+	    		break;
+	    	},
+	    	std::cmp::Ordering::Greater => println!("Too big!"),
+	    }
+	}
+
 }
