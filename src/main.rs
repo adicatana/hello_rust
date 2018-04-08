64019baefcbaa35a1185c050f0a71599e193f1aa
Adi Catana <adrian.catana15@imperial.ac.uk>
2018-04-08 17:03:23 +0300

    Working with errors and files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate hello_rust;
 
+use std::fs::File;
+use std::io::Read;
+use std::io::ErrorKind;
+use std::io;
+
 // use hello_rust::guessing_game;
 // use hello_rust::fibonacci;
+
+const FILE_IN: &str = "text.in";
 
 pub fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
@@ -10,9 +17,54 @@ pub fn plus_one(x: Option<i32>) -> Option<i32> {
     }
 }
 
+pub fn read_username_from_file(file_name: &str) -> Result<String, io::Error> {
+
+    // let mut f = File::open(file_name)?;
+    // let f = File::open(file_name);
+
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+
+    let mut s = String::new();
+
+    File::open(file_name)?.read_to_string(&mut s)?;
+    Ok(s)
+    // match f.read_to_string(&mut s) {
+        // Ok(_) => Ok(s),
+        // Err(e) => Err(e),
+    // }
+
+}
+
 fn main() {
 
-    
+    let f = File::open(FILE_IN);
+
+    let _f = match f {
+        Ok(file) => file,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            match File::create(FILE_IN) {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        "Tried to create a file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
+        Err(e) => {
+            panic!(
+                "There was a problem opening the file: {:?}",
+                e
+            )
+        }
+    };
+
+    let _f = File::open(FILE_IN).unwrap();
+    let _f = File::open(FILE_IN).expect("No such file.");
 
     // guessing_game::guessing_game();
 
